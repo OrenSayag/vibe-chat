20e35e387fdbeed435da1494eb1d69a9fd0ac725
Oren Sayag <orensayag500@gmail.com>
2025-01-13 08:53:12 +0200

    ✨ added vibes component library, ui/components lib

---
+++ libs/ui/components/src/lib/button/index.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { ComponentPropsWithoutRef, FC } from 'react';
+import { cn } from '@monday-whatsapp/utils';
+
+import { Button as VibesButton } from "@vibe/core";
+
+type Props = ComponentPropsWithoutRef<typeof VibesButton> & {
+  className?: string;
+}
+
+export const Button: FC<Props> = ({ className, ...props }) => {
+  return (
+    <>
+      <VibesButton className={cn(className)} {...props} />
+    </>
+  );
+};

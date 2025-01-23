eb74e0c435eaf41ae0ba8bd57b5e702a39f54c6d
Oren Sayag <orensayag500@gmail.com>
2025-01-23 08:55:17 +0200

    ♻️ mw-99 - whatsapp refactor - db - subscriptions - remove greenApiInfo

--- libs/db/src/schema.ts
+++ libs/db/src/schema.ts
@@ -1,13 +1,9 @@
 import { int, json, mysqlTable, timestamp } from 'drizzle-orm/mysql-core';
-import {
-  GreenApiInstanceInfo,
-  SubscriptionInfo,
-} from '@monday-whatsapp/shared-types';
+import { SubscriptionInfo } from '@monday-whatsapp/shared-types';
 
 export const subscriptions = mysqlTable('subscriptions', {
   id: int('id').primaryKey().autoincrement(),
   createdAt: timestamp('created_at').defaultNow(),
   updatedAt: timestamp('updated_at').onUpdateNow(),
   info: json('info').$type<SubscriptionInfo>().notNull(),
-  greenApiInfo: json('green_api_info').$type<GreenApiInstanceInfo>(),
 });

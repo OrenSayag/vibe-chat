dfd6e9752c989e88cd8031833077dc29cd24b2ba
Oren Sayag <orensayag500@gmail.com>
2025-02-10 07:52:45 +0200

    fixed typo

--- apps/dashboard/src/app/[locale]/dashboard/[subscriptionId]/integration/whatsapp/templates/page.tsx
+++ apps/dashboard/src/app/[locale]/dashboard/[subscriptionId]/integration/whatsapp/templates/page.tsx
@@ -4,17 +4,18 @@ import { getSubscription } from '@vibe-chat/next-services/server';
 import { WhatsappCloudStatus } from '@vibe-chat/shared-types';
 
 export default async function TemplatesPage({
-    params: { subscriptionId },
+  params: { subscriptionId },
 }: {
-    params: { subscriptionId: string };
+  params: { subscriptionId: string };
 }) {
-    const { info } = await getSubscription({ subscriptionId });
+  const { info } = await getSubscription({ subscriptionId });
 
-    if (info.integrations.whatsappCloudInfo?.status !== WhatsappCloudStatus.SIGNED) {
-        throw new Error('Whatsapp is not signed');
-    }
+  if (
+    info.integrations.whatsappCloudInfo?.status !== WhatsappCloudStatus.SIGNED
+  ) {
+    throw new Error('Whatsapp is not signed');
+  }
 
-    const props = await getWhatsappIntegrationProps(info, subscriptionId, true);
-    return <WhatsappIntegrationTemplateProvider {...props} />;
+  const props = await getWhatsappIntegrationProps(info, subscriptionId, true);
+  return <WhatsappIntegrationTemplateProvider {...props} />;
 }
-˝
